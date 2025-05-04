c084ea9baaf1f53151e3360201995ee3c7210101
Saad Ahmad <saadehmd@gmail.com>
2025-05-04 15:15:26 +0000

    Clean-up trigger_plotter utility and make plots more readable with translucent fills and right z-order.

--- include/ros2bag_triggered/trigger_plotter.hpp
+++ include/ros2bag_triggered/trigger_plotter.hpp
@@ -8,6 +8,7 @@
 #include <map>
 #include <set>
 #include <sstream>
+#include <any>
 
 template< typename T, size_t HexLen=6 >
 std::string to_hex( T i )
@@ -20,8 +21,11 @@ std::string to_hex( T i )
 namespace ros2bag_triggered::utils
 {
 
-using TimePointNs = std::chrono::time_point<std::chrono::system_clock, std::chrono::nanoseconds>;
-using NanoSec = std::chrono::nanoseconds;
+using Ns = std::chrono::nanoseconds;
+
+template<typename durationT>
+using TimePoint = std::chrono::time_point<std::chrono::system_clock, durationT>;
+
 
 
 struct PulsePlot
@@ -48,7 +52,6 @@ inline void sort_trigger_pulses(const TriggerPulseMap& trigger_pulse_map, Trigge
     for(const auto& [trigger_name, trigger_pulses] : trigger_pulse_map)
     {
         std::string color = "#" + to_hex(trigger_pulse_map.hash_function()(trigger_name) % max_colors);
-        std::cout << "Trigger Name: " << trigger_name << " Color: " << color << std::endl;
         for (const auto& pulse : trigger_pulses)
         {
             trigger_pulse_plots.emplace_back(trigger_name, color, pulse);
@@ -59,28 +62,45 @@ inline void sort_trigger_pulses(const TriggerPulseMap& trigger_pulse_map, Trigge
     std::sort(trigger_pulse_plots.begin(), trigger_pulse_plots.end(), trigger_comparator);
 }
 
+inline std::string get_human_readable_datetime(const TimePoint<Ns>& time_point)
+{
+    auto timezone = std::chrono::current_zone();
+    auto local_datetime = timezone->to_local(time_point);
+    return std::format("{:%Y-%m-%d %H:%M:%S} {}", local_datetime, timezone->name());
+}
+
+inline std::pair<int, int> get_plot_idx_from_pulse(const TriggerPulse& trigger_pulse, 
+                                        const TimePoint<Ns>& bag_start_time, 
+                                        const TimePoint<Ns>& bag_end_time, 
+                                        double time_axs_resolution,
+                                        size_t time_axis_size)
+{
+    auto trigger_start_time = std::chrono::duration_cast<std::chrono::seconds>(TimePoint(Ns(trigger_pulse.start_time_)) - bag_start_time);
+    auto trigger_end_time = std::chrono::duration_cast<std::chrono::seconds>(TimePoint(Ns(trigger_pulse.end_time_)) - bag_start_time);
+    int start_idx = std::round( trigger_start_time.count() / time_axs_resolution);
+    int end_idx = std::round( trigger_end_time.count() / time_axs_resolution);
+    start_idx = std::clamp<int>(start_idx, 0, time_axis_size);
+    end_idx = std::clamp<int>(end_idx, start_idx, time_axis_size);
+    return {start_idx, end_idx};
+}
+
 inline void plot_triggers(const TriggerPulseMap& trigger_pulse_map,
-                          const TimePointNs& bag_start_time, 
-                          const TimePointNs& bag_end_time, 
+                          const TimePoint<Ns>& bag_start_time, 
+                          const TimePoint<Ns>& bag_end_time, 
                           const std::filesystem::path& trigger_plot_file)
 {
     double bag_duration = std::chrono::duration_cast<std::chrono::seconds>(bag_end_time - bag_start_time).count();
-
-    auto timezone = std::chrono::current_zone();
-    auto start_datetime = timezone->to_local(bag_start_time);
-    auto start_datetime_str = std::format("{:%Y-%m-%d %H:%M:%S} {}", start_datetime, timezone->name());
-    auto end_datetime = timezone->to_local(TimePointNs(bag_end_time));
-    auto end_datetime_str = std::format("{:%Y-%m-%d %H:%M:%S} {}", end_datetime, timezone->name());
+    auto start_datetime_str = get_human_readable_datetime(bag_start_time);
+    auto end_datetime_str = get_human_readable_datetime(bag_end_time);
 
     // Restrict the x-ticks for ease of plot-visualization.
     const size_t x_divs = 20;
-    std::vector<double> xticks{};
     std::vector<double> yticks{0.0, 1.0};
+    std::vector<double> xticks{};
+    const double precision = 0.01;
     for (double x_div = 1; x_div <= x_divs; x_div++)
     {
-        const double precision = 0.01;
-        auto x_tick = std::round(((x_div * bag_duration) / x_divs) / precision) * precision;
-        xticks.push_back(x_tick);
+        xticks.push_back(std::round(((x_div * bag_duration) / x_divs) / precision) * precision);
     }
     // @todo: the std::ranges::iota_view::to<vector>() is not available in C++20 and not standardized for all
     // compilers yet in C++23. So this could be converted to the ranges method instead of for loop in future.
@@ -97,6 +117,8 @@ inline void plot_triggers(const TriggerPulseMap& trigger_pulse_map,
         TriggerPulsePlots trigger_pulse_plots;
         sort_trigger_pulses(trigger_pulse_map, trigger_pulse_plots);
 
+        std::vector<std::function<void()>> plot_fill_calls;
+        
         for (const auto& trigger_pulse_plot : trigger_pulse_plots)
         {
             std::map<std::string, std::string> plot_options;
@@ -112,10 +134,7 @@ inline void plot_triggers(const TriggerPulseMap& trigger_pulse_map,
             std::vector<double> trigger_axis(time_axis.size(), -1.0);
 
             auto& trigger_pulse = trigger_pulse_plot.trigger_pulse_;
-            int start_idx = std::round((trigger_pulse.start_time_ - bag_start_time.time_since_epoch().count()) * 1e-9 / dt);
-            int end_idx = std::round((trigger_pulse.end_time_ - bag_start_time.time_since_epoch().count()) * 1e-9 / dt);
-            start_idx = std::clamp<int>(start_idx, 0, time_axis.size());
-            end_idx = std::clamp<int>(end_idx, start_idx, time_axis.size());
+            auto [start_idx, end_idx] = get_plot_idx_from_pulse(trigger_pulse, bag_start_time, bag_end_time, dt, time_axis.size());
 
             auto x_fill = std::vector<double>(time_axis.begin() + start_idx, time_axis.begin() + end_idx);
             std::vector<double> y_fill(x_fill.size(), 1.0);
@@ -126,10 +145,24 @@ inline void plot_triggers(const TriggerPulseMap& trigger_pulse_map,
             }
 
             matplotlibcpp::plot(time_axis, trigger_axis, plot_options);
-            matplotlibcpp::fill_between(x_fill, std::vector<double>(x_fill.size(), 0.95), y_fill,
-                                        std::map<std::string, std::string>{{"color", trigger_pulse_plot.trigger_color_}});
-            
+
+            plot_fill_calls.push_back([&trigger_pulse_plot, x_fill, y_fill]() {
+                matplotlibcpp::fill_between(x_fill, std::vector<double>(x_fill.size(), 0.95), y_fill, {{"color", trigger_pulse_plot.trigger_color_}});
+                matplotlibcpp::fill_between(x_fill, std::vector<double>(x_fill.size(), 0.0), y_fill, {{"color", trigger_pulse_plot.trigger_color_}});
+            });
         }
+
+        // This is a workaround to set the z-order and alpha values as they are non-string values and newer matplotlib versions 
+        // can't parse them from string to int/float/double... types. While the matplotlibcpp library only accepts kwargs as map<string, string>.
+        // This currently an open issue in the matplotlibcpp library: https://github.com/lava/matplotlib-cpp/issues/75
+        PyRun_SimpleString("import matplotlib.pyplot as plt");
+        for (const auto& plot_fill_call : plot_fill_calls)
+        {
+            plot_fill_call();
+            PyRun_SimpleString("plt.gca().collections[-2].set_zorder(3)");
+            PyRun_SimpleString("plt.gca().collections[-1].set_alpha(0.3)");
+        }
+        
 
         matplotlibcpp::xlabel("Time (s)");
         matplotlibcpp::ylabel("Trigger Pulses");
